4f26f0cfa359fbe3faa1c949ed6c1b8f9843a981
hanzala <ansarihanzala1101@gmail.com>
2022-06-11 11:49:45 +0530

    unknown commit

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,67 +1,79 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Loginpage extends StatelessWidget {
+class Loginpage extends StatefulWidget {
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  String name = "";
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Color.alphaBlend(Colors.white, Colors.blue),
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/chu.jpg",
-              fit: BoxFit.scaleDown,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Rehan umar:25 kaam:coatmaker hobbies: logo ko bewakuf bana na",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter the text",
-                      labelText: "Enter your name",
-                    ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/nature.jpg",
+                  fit: BoxFit.scaleDown,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text("Nature is god gifted do not destroy $name",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.redAccent,
+                    )),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter the text",
+                          labelText: "Enter your name",
+                        ),
+                      ),
+                      // SizedBox(
+                      //   height: 10,
+                      // ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter the text",
+                          labelText: "Enter your password",
+                        ),
+                      ),
+                    ],
                   ),
-                  // SizedBox(
-                  //   height: 10,
-                  // ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter the text",
-                      labelText: "Enter your password",
-                    ),
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                
+                // ElevatedButton(
+                //   child: Text("login"),
+                //   style: TextButton.styleFrom(
+                //       primary: Color.lerp(Colors.black, Colors.black, 5.3),
+                //       shadowColor: Color.alphaBlend(
+                //           Colors.deepPurple, Colors.greenAccent),
+                //       fixedSize: Size(100, 20)),
+                //   onPressed: () {
+                //     Navigator.pushNamed(context, MyRoutes.HomeRoutes);
+                //   },
+                // ),
+              ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              child: Text("login"),
-              style: TextButton.styleFrom(
-                  primary: Color.lerp(Colors.black, Colors.black, 5.3),
-                  shadowColor:
-                      Color.alphaBlend(Colors.deepPurple, Colors.greenAccent),
-                  fixedSize: Size(100, 20)),
-              onPressed: () {
-                print("good job");
-              },
-            ),
-          ],
+          ),
         ));
   }
 }
